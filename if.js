b7753053e50c7a45b612c6b11fa3094c558d3185
IlyassGueddi <gueddiilyass99@gmail.com>
2025-10-06 17:58:30 +0100

    if statement

---
+++ if.js
@@ -0,0 +1,24 @@
+// this is the right methode to write if statement in javascript //
+/*
+
+if (condition){
+
+}else if (codition) {
+
+}else{
+    
+}
+
+*/
+
+// ---------- Example ---------- //
+
+let age =  19
+
+if (age >= 18 ) {
+    console.log("you are an adulte")
+}else if ( age < 18 ) {
+    console.log("you still a boy")
+}else{
+    console.log("invalid value")
+}
