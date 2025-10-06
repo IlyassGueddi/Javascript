c9f6219b9022fb0207664831a6e2101a79c37c84
IlyassGueddi <gueddiilyass99@gmail.com>
2025-10-06 17:49:49 +0100

    for loop in js

---
+++ for.js
@@ -0,0 +1,15 @@
+// we write for loop in javascript as //
+// for (start; condition; step) //
+
+for (i = 0; i < 5; i ++){
+    console.log(i);
+}
+
+
+// -------- an other exxample -------- //
+
+let fruits = ["banana", "avocado", "apple", "orange"]
+
+for (i = fruits.length - 1 ; i >= 0 ; i --){
+    console.log(fruits[i]);
+}
